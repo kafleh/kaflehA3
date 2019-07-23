8608f10382e3358ca3de6017683c04913e644b5b
kafleh <52709789+kafleh@users.noreply.github.com>
2019-07-23 16:25:21 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,30 @@
+import { Book } from '../../app/bookInfo';
+
+export const MYBOOKS:Book[]=
+    [
+        {   bookName: "Muna Madan",
+            authorName: "L P Devkota",
+            bookGenre: "Novel",
+            yearPublished: 1950,
+            picture: "munamadan.jpg" 
+        },
+        {   bookName: "Hari Bahadur",
+            authorName: "Hari Bansha Acharya",
+            bookGenre: "Autobiography",
+            yearPublished: 2017,
+            picture: "chinaHaraekoManchhe.jpg" 
+        },
+        {   bookName: "Mahako Ma",
+            authorName: "Madan Krishna Stha",
+            bookGenre: "Biography",
+            yearPublished: 2018,
+            picture: "mahakoMa.jpg" 
+        },
+        {   bookName: "Half Girlfriend",
+            authorName: "Chetan Bhagat",
+            bookGenre: "Novel",
+            yearPublished: 2012,
+            picture: "halfGirlFriend.jpg" 
+        }
+        
+    ]
